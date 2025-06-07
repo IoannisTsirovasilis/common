05c34ff70129b4f3b2f160254e315d70e01e9697
Ioannis Tsirovasilis <ioannistsirovasilis@gmail.com>
2025-06-07 21:48:22 +0300

    Export next from next-core

--- packages/next-core/src/index.ts
+++ packages/next-core/src/index.ts
@@ -4,3 +4,4 @@ export * from "./lib/logger";
 export * from "./lib/interfaces/HandleApiRequestOptions";
 export * from "./lib/constants/constants";
 export * from "./lib/utils/utils";
+export * from "next";
