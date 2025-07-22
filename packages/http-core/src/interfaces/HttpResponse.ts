50e2a4aa7f1cc8bbdb960e04a6d7eecd493a78af
Ioannis Tsirovasilis <ioannistsirovasilis@gmail.com>
2025-07-22 18:42:35 +0300

    feat(http-core): add optional headers property to HttpResponse interface

--- packages/http-core/src/interfaces/HttpResponse.ts
+++ packages/http-core/src/interfaces/HttpResponse.ts
@@ -2,6 +2,7 @@ import { ResponseCode } from "../enums/ResponseCode.js";
 import { ResponseData } from "./ResponseData.js";
 
 export interface HttpResponse<M extends ResponseData> {
+  headers?: Headers;
   data: M;
   message: string;
   status: ResponseCode;
