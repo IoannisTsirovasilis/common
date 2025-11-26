8042543894cbfc63408bbd9f4fca0219d7d7c310
Ioannis Tsirovasilis <ioannistsirovasilis@gmail.com>
2025-11-26 12:17:59 +0200

    feat(next-core): release version 6.1.0, adding `NextRequestProps` interface and updating package version in package.json and package-lock.json

--- packages/next-core/CHANGELOG.md
+++ packages/next-core/CHANGELOG.md
@@ -7,6 +7,11 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [6.1.0] - 2025-11-26
+
+### Added
+- Export `NextRequestProps` interface
+
 ## [6.0.0] - 2025-10-23
 
 ### Breaking Changes
--- packages/next-core/src/index.ts
+++ packages/next-core/src/index.ts
@@ -4,3 +4,4 @@ export { logger } from "./lib/logger.js";
 export { HandleApiRequestOptions } from "./lib/interfaces/HandleApiRequestOptions.js";
 export { NextRequestParts } from "./lib/interfaces/NextRequestParts.js";
 export { extractRequestParts } from "./lib/utils/utils.js";
+export { NextRequestProps } from "./lib/interfaces/NextRequestProps.js";
